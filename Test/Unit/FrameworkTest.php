ac7ceb19cd4b1c21f9bc486ae93ceab91e080b90
Juergen Eger <juergen.eger@wirecard.com>
2017-04-11 15:39:35 +0200

    restructured project to better reflect magento 2

---
+++ Test/Unit/FrameworkTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: juergen.eger
+ * Date: 11.04.2017
+ * Time: 14:52
+ */
+
+namespace Wirecard\ElasticEngine\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FrameworkTest extends TestCase
+{
+
+    public function testFramework()
+    {
+        $this->assertFalse(false);
+    }
+}
