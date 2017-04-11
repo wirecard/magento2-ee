f4f6d58ab1ca3995b445dabee0703a92c1f58414
Juergen Eger <juergen.eger@wirecard.com>
2017-04-11 16:36:03 +0200

    introducing new code style fixer php-cs-fixer

--- Test/Unit/FrameworkTest.php
+++ Test/Unit/FrameworkTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: juergen.eger
- * Date: 11.04.2017
- * Time: 14:52
- */
+
 
 namespace Wirecard\ElasticEngine\Test\Unit;
 
@@ -12,7 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class FrameworkTest extends TestCase
 {
-
     public function testFramework()
     {
         $this->assertFalse(false);
