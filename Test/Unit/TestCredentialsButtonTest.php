b80a84b15d7b2414da2b1f69a7259c9d9692d382
Horst Fickel <horst.fickel@wirecard.com>
2017-04-12 10:57:31 +0200

    first test

---
+++ Test/Unit/TestCredentialsButtonTest.php
@@ -0,0 +1,116 @@
+<?php
+
+
+namespace Wirecard\ElasticEngine\Test\Unit\Block\Widget;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Backend\Block\Widget\Button;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Filesystem;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\Url;
+use Magento\Framework\View\Element\Template\File\Resolver;
+use Magento\TestFramework\EventManager;
+use Monolog\Logger;
+use Wirecard\ElasticEngine\Block\Widget\TestCredentialsButton;
+
+class TestCredentialsButtonTest extends \PHPUnit_Framework_TestCase
+{
+    const BUTTON = '<button></button>';
+    /**
+     * @var TestCredentialsButton
+     */
+    private $instance;
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $storeManagerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $layoutMock;
+
+    public function setUp()
+    {
+        $this->objectManager = new ObjectManager($this);
+
+        $directory = $this->getMockForAbstractClass(\Magento\Framework\Filesystem\Directory\ReadInterface::class);
+        $filesystem = $this->getMock(Filesystem::class, ['getDirectoryRead'], [], '', false);
+        $filesystem->method('getDirectoryRead')->willReturn($directory);
+        $eventManager = $this->getMock(EventManager::class, ['dispatch'], [], '', false);
+        $appState = $this->getMock(\Magento\Framework\App\State::class, ['getAreaCode'], [], '', false);
+        $resolver = $this->getMock(Resolver::class, ['getTemplateFileName'], [], '', false);
+        $validator = $this->getMock(\Magento\Framework\View\Element\Template\File\Validator::class, ['isValid'], [], '',
+            false);
+        $logger = $this->getMock(Logger::class, ['critical'], [], '', false);
+        $urlBuilder = $this->getMock(Url::class, ['getUrl'], [], '', false);
+
+        $context = $this->getMock(Context::class, [
+            'getFilesystem',
+            'getEventManager',
+            'getAppState',
+            'getResolver',
+            'getValidator',
+            'getLogger',
+            'getUrlBuilder'
+        ], [], '', false);
+        $context->method('getFilesystem')->willReturn($filesystem);
+        $context->method('getEventManager')->willReturn($eventManager);
+        $context->method('getAppState')->willReturn($appState);
+        $context->method('getResolver')->willReturn($resolver);
+        $context->method('getValidator')->willReturn($validator);
+        $context->method('getLogger')->willReturn($logger);
+        $context->method('getUrlBuilder')->willReturn($urlBuilder);
+
+        $data = [
+            'context' => $context,
+        ];
+
+        $this->instance = $this->objectManager->getObject(TestCredentialsButton::class, $data);
+
+        $this->layoutMock = $this->getMockBuilder('Magento\Framework\View\Layout')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $button = $this->getMockBuilder(Button::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $button->method('setData')->willReturn($button);
+        $button->method('toHtml')->willReturn(self::BUTTON);
+
+        $this->layoutMock->method('createBlock')->willReturn($button);
+        $this->instance->setLayout($this->layoutMock);
+    }
+
+    public function testConstructor()
+    {
+        $expected = 'Wirecard_ElasticEngine::system/config/form/test_credentials_button.phtml';
+        $this->assertEquals($expected, $this->instance->getTemplate());
+    }
+
+    public function testRender()
+    {
+        $element = $this->getMock(AbstractElement::class, ['unScope', 'getHtmlId', 'getScope', 'getLabel'], [], '',
+            false);
+
+        $this->assertEquals('<tr id="row_"><td class="label"><label for=""><span></span></label></td><td class="value"></td><td class=""></td></tr>',
+            $this->instance->render($element));
+    }
+
+    public function testGetAjaxUrl()
+    {
+        $this->assertEquals(null, $this->instance->getAjaxUrl());
+    }
+
+    public function testGetButtonHtml()
+    {
+        $this->assertEquals(self::BUTTON, $this->instance->getButtonHtml());
+    }
+}
---
+++ Test/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/magento/community-edition/app/functions.php';
