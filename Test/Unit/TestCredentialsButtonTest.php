9aeb44daf17322e2599a845fcc5a3b86d3bb23c1
Horst Fickel <horst.fickel@wirecard.com>
2017-04-12 11:02:22 +0200

    fixed bootstrap loading

--- Test/Unit/TestCredentialsButtonTest.php
+++ Test/Unit/TestCredentialsButtonTest.php
@@ -1,15 +1,47 @@
 <?php
-
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard Central Eastern Europe GmbH
+ * (abbreviated to Wirecard CEE) and are explicitly not part of the Wirecard CEE range of
+ * products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License Version 2 (GPLv2) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard CEE does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard CEE does not guarantee their full
+ * functionality neither does Wirecard CEE assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard CEE does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
 
 namespace Wirecard\ElasticEngine\Test\Unit\Block\Widget;
 
 use Magento\Backend\Block\Template\Context;
 use Magento\Backend\Block\Widget\Button;
+use Magento\Framework\App\State;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\ReadInterface;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Framework\Url;
 use Magento\Framework\View\Element\Template\File\Resolver;
+use Magento\Framework\View\Element\Template\File\Validator;
 use Magento\TestFramework\EventManager;
 use Monolog\Logger;
 use Wirecard\ElasticEngine\Block\Widget\TestCredentialsButton;
@@ -41,13 +73,13 @@ class TestCredentialsButtonTest extends \PHPUnit_Framework_TestCase
     {
         $this->objectManager = new ObjectManager($this);
 
-        $directory = $this->getMockForAbstractClass(\Magento\Framework\Filesystem\Directory\ReadInterface::class);
+        $directory = $this->getMockForAbstractClass(ReadInterface::class);
         $filesystem = $this->getMock(Filesystem::class, ['getDirectoryRead'], [], '', false);
         $filesystem->method('getDirectoryRead')->willReturn($directory);
         $eventManager = $this->getMock(EventManager::class, ['dispatch'], [], '', false);
-        $appState = $this->getMock(\Magento\Framework\App\State::class, ['getAreaCode'], [], '', false);
+        $appState = $this->getMock(State::class, ['getAreaCode'], [], '', false);
         $resolver = $this->getMock(Resolver::class, ['getTemplateFileName'], [], '', false);
-        $validator = $this->getMock(\Magento\Framework\View\Element\Template\File\Validator::class, ['isValid'], [], '',
+        $validator = $this->getMock(Validator::class, ['isValid'], [], '',
             false);
         $logger = $this->getMock(Logger::class, ['critical'], [], '', false);
         $urlBuilder = $this->getMock(Url::class, ['getUrl'], [], '', false);
