6e1e4e0bf35fe6532c3e67798788887ebd0991e1
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-11-09 15:29:58 +0100

    Implement payment method sofortbanking

--- Gateway/Request/SofortTransactionFactory.php
+++ Gateway/Request/SofortTransactionFactory.php
@@ -34,38 +34,22 @@ namespace Wirecard\ElasticEngine\Gateway\Request;
 
 use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\UrlInterface;
-use Magento\Payment\Gateway\ConfigInterface;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Wirecard\PaymentSdk\Exception\MandatoryFieldMissingException;
-use Wirecard\PaymentSdk\Transaction\PayPalTransaction;
+use Wirecard\PaymentSdk\Transaction\SofortTransaction;
 use Wirecard\PaymentSdk\Transaction\Transaction;
 
 /**
- * Class PayPalTransactionFactory
+ * Class SofortTransactionFactory
  * @package Wirecard\ElasticEngine\Gateway\Request
  */
-class PayPalTransactionFactory extends TransactionFactory
+class SofortTransactionFactory extends TransactionFactory
 {
     /**
-     * @var BasketFactory
-     */
-    private $basketFactory;
-
-    /**
-     * @var PayPalTransaction
+     * @var SofortTransaction
      */
     protected $transaction;
-
-    /**
-     * @var AccountHolderFactory
-     */
-    private $accountHolderFactory;
-
-    /**
-     * @var ConfigInterface
-     */
-    private $methodConfig;
 
     /**
      * @var StoreManagerInterface
@@ -73,30 +57,21 @@ class PayPalTransactionFactory extends TransactionFactory
     private $storeManager;
 
     /**
-     * PayPalTransactionFactory constructor.
+     * SofortTransactionFactory constructor.
      * @param UrlInterface $urlBuilder
      * @param ResolverInterface $resolver
      * @param StoreManagerInterface $storeManager
      * @param Transaction $transaction
-     * @param BasketFactory $basketFactory
-     * @param AccountHolderFactory $accountHolderFactory
-     * @param ConfigInterface $methodConfig
      */
     public function __construct(
         UrlInterface $urlBuilder,
         ResolverInterface $resolver,
         StoreManagerInterface $storeManager,
-        Transaction $transaction,
-        BasketFactory $basketFactory,
-        AccountHolderFactory $accountHolderFactory,
-        ConfigInterface $methodConfig
+        Transaction $transaction
     ) {
         parent::__construct($urlBuilder, $resolver, $transaction);
 
         $this->storeManager = $storeManager;
-        $this->basketFactory = $basketFactory;
-        $this->accountHolderFactory = $accountHolderFactory;
-        $this->methodConfig = $methodConfig;
     }
 
     /**
@@ -110,30 +85,11 @@ class PayPalTransactionFactory extends TransactionFactory
         parent::create($commandSubject);
 
         /** @var PaymentDataObjectInterface $payment */
-        $payment = $commandSubject[self::PAYMENT];
-        $order = $payment->getOrder();
-        $billingAddress = $order->getBillingAddress();
 
-        $this->transaction->setAccountHolder($this->accountHolderFactory->create($billingAddress));
-        $this->transaction->setShipping($this->accountHolderFactory->create($order->getShippingAddress()));
-        //$this->transaction->setOrderNumber($this->orderId);
-        $this->transaction->setOrderDetail(sprintf(
-            '%s %s %s',
-            $billingAddress->getEmail(),
-            $billingAddress->getFirstname(),
-            $billingAddress->getLastname()
-        ));
-
-        if ($this->methodConfig->getValue('send_shopping_basket')) {
-            $this->transaction->setBasket($this->basketFactory->create($order));
-        }
-
-        if ($this->methodConfig->getValue('send_descriptor')) {
-            $this->transaction->setDescriptor(sprintf('%s %s',
+        $this->transaction->setDescriptor(sprintf('%s %s',
                 substr($this->storeManager->getStore()->getName(), 0, 9),
                 $this->orderId
             ));
-        }
 
         return $this->transaction;
     }
--- view/frontend/web/js/view/payment/method-renderer/sofort.js
+++ view/frontend/web/js/view/payment/method-renderer/sofort.js
@@ -31,32 +31,35 @@
 
 define(
     [
-        'uiComponent',
-        'Magento_Checkout/js/model/payment/renderer-list'
+        'jquery',
+        'Wirecard_ElasticEngine/js/view/payment/method-renderer/default',
+        'mage/url'
     ],
-    function (
-        Component,
-        rendererList
-    ) {
+    function ($, Component, url) {
         'use strict';
-        rendererList.push(
-            {
-                type: 'wirecard_elasticengine_paypal',
-                component: 'Wirecard_ElasticEngine/js/view/payment/method-renderer/default'
+        return Component.extend({
+            defaults: {
+                template: 'Wirecard_ElasticEngine/payment/method-default',
+                redirectAfterPlaceOrder: false
             },
-            {
-                type: 'wirecard_elasticengine_creditcard',
-                component: 'Wirecard_ElasticEngine/js/view/payment/method-renderer/creditcard'
-            },
-            {
-                type: 'wirecard_elasticengine_maestro',
-                component: 'Wirecard_ElasticEngine/js/view/payment/method-renderer/creditcard'
-            },
-            {
-                type: 'wirecard_elasticengine_sofort',
-                component: 'Wirecard_ElasticEngine/js/view/payment/method-renderer/sofort'
+            afterPlaceOrder: function () {
+                $.get(url.build("wirecard_elasticengine/frontend/callback"), function (data) {
+                    if (data['form-url']) {
+                        var form = $('<form />', {action: data['form-url'], method: data['form-method']});
+
+                        for (var i = 0; i < data['form-fields'].length; i++) {
+                            form.append($('<input />', {
+                                type: 'hidden',
+                                name: data['form-fields'][i]['key'],
+                                value: data['form-fields'][i]['value']
+                            }));
+                        }
+                        form.appendTo('body').submit();
+                    } else {
+                        window.location.replace(data['redirect-url']);
+                    }
+                });
             }
-        );
-        return Component.extend({});
+        });
     }
 );
