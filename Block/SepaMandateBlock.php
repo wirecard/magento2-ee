83d33ec1fb9bb42ca9d6f497638ce0d700d39a1d
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-11-10 08:19:30 +0100

    #55 create sepa mandate block

---
+++ Block/SepaMandateBlock.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard Central Eastern Europe GmbH
+ * (abbreviated to Wirecard CEE) and are explicitly not part of the Wirecard CEE range of
+ * products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License Version 2 (GPLv2) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard CEE does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard CEE does not guarantee their full
+ * functionality neither does Wirecard CEE assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard CEE does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+namespace Wirecard\ElasticEngine\Block;
+
+use Magento\Checkout\Model\Session;
+use Magento\Framework\View\Element\Template;
+use Magento\Payment\Gateway\ConfigInterface;
+
+class SepaMandateBlock extends Template
+{
+    /**
+     * @var ConfigInterface
+     */
+    private $sepaConfig;
+
+    /**
+     * @var Template\Context $context
+     */
+    private $context;
+
+    /**
+     * @var Session $session
+     */
+    private $session;
+
+    /**
+     * Constructor
+     *
+     * @param Template\Context $context
+     * @param ConfigInterface $methodConfig
+     * @param Session $session
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        ConfigInterface $methodConfig,
+        Session $session,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->sepaConfig = $methodConfig;
+        $this->context = $context;
+        $this->session = $session;
+    }
+
+    public function getCreditorId()
+    {
+        return $this->sepaConfig->getValue('creditor_id');
+    }
+
+    public function getCreditorName()
+    {
+        return $this->sepaConfig->getValue('creditor_name');
+    }
+
+    public function getMandateId()
+    {
+
+        return $this->getCreditorId() . "-" . $this->session->getQuoteId() . "-" . strtotime(date("Y-m-d H:i:s"));
+    }
+
+    public function getStoreCity()
+    {
+        return $this->sepaConfig->getValue('creditor_city');
+    }
+}
