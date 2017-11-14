13b5e98abfc837d0d8ab4ba3cf0c5e1355aecfab
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-11-14 15:54:09 +0100

    #65 change transaction type from payment to capture

--- Controller/Frontend/Notify.php
+++ Controller/Frontend/Notify.php
@@ -224,7 +224,7 @@ class Notify extends Action
         $transactionType = $response->getTransactionType();
 
         if ('debit' === $transactionType) {
-            $transactionType = 'payment';
+            $transactionType = 'capture';
         }
 
         $payment->addTransaction($transactionType);
