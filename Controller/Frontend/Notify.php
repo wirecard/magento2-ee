79888a1711cb808dadc04fa139917e77e616bbef
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-12-12 12:25:05 +0100

    #92 Remove uncommented code to prevent failed tests

--- Controller/Frontend/Notify.php
+++ Controller/Frontend/Notify.php
@@ -156,13 +156,6 @@ class Notify extends Action
 
         if ($response instanceof SuccessResponse) {
             if ($order->getStatus() !== Order::STATE_COMPLETE) {
-                /*if ($response->isValidSignature()) {
-                    $this->updateOrderState($order, Order::STATE_PROCESSING);
-                } else {
-                    $this->updateOrderState($order, Order::STATUS_FRAUD);
-                    $this->logger->warning(sprintf('Possible fraud detected in notification for order id: %s',
-                        $orderId));
-                }*/
                 $this->updateOrderState($order, Order::STATE_PROCESSING);
             }
 
