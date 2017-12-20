6e11cbaab64bbff2ce018a305ed7894663f6bb95
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2017-12-20 14:00:50 +0100

    #100 Add sepa, sofort and transactionfactory tests for new constructor

--- Test/Unit/Gateway/Request/TransactionFactoryUTest.php
+++ Test/Unit/Gateway/Request/TransactionFactoryUTest.php
@@ -33,10 +33,17 @@ namespace Wirecard\ElasticEngine\Test\Unit\Gateway\Request;
 
 use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\UrlInterface;
+use Magento\Payment\Gateway\ConfigInterface;
 use Magento\Payment\Gateway\Data\OrderAdapterInterface;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Wirecard\ElasticEngine\Gateway\Request\AccountHolderFactory;
+use Wirecard\ElasticEngine\Gateway\Request\BasketFactory;
 use Wirecard\ElasticEngine\Gateway\Request\TransactionFactory;
+use Wirecard\PaymentSdk\Entity\AccountHolder;
 use Wirecard\PaymentSdk\Entity\Amount;
+use Wirecard\PaymentSdk\Entity\Basket;
 use Wirecard\PaymentSdk\Entity\Redirect;
 use Wirecard\PaymentSdk\Transaction\Operation;
 use Wirecard\PaymentSdk\Transaction\PayPalTransaction;
@@ -51,6 +58,14 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
     private $payment;
 
     private $order;
+
+    private $storeManager;
+
+    private $basketFactory;
+
+    private $accountHolderFactory;
+
+    private $config;
 
     private $commandSubject;
 
@@ -70,6 +85,20 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()->getMock();
         $this->payment->method('getOrder')->willReturn($this->order);
 
+        $store = $this->getMockBuilder(StoreInterface::class)->disableOriginalConstructor()->getMock();
+        $store->method('getName')->willReturn('My shop name');
+
+        $this->storeManager = $this->getMockBuilder(StoreManagerInterface::class)->disableOriginalConstructor()->getMock();
+        $this->storeManager->method('getStore')->willReturn($store);
+
+        $this->basketFactory = $this->getMockBuilder(BasketFactory::class)->disableOriginalConstructor()->getMock();
+        $this->basketFactory->method('create')->willReturn(new Basket());
+
+        $this->accountHolderFactory = $this->getMockBuilder(AccountHolderFactory::class)->disableOriginalConstructor()->getMock();
+        $this->accountHolderFactory->method('create')->willReturn(new AccountHolder());
+
+        $this->config = $this->getMockBuilder(ConfigInterface::class)->disableOriginalConstructor()->getMock();
+
         $this->commandSubject = ['payment' => $this->payment];
     }
 
@@ -78,7 +107,8 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
      */
     public function testCreateThrowsExceptionWithoutPayment()
     {
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction());
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction(),
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $transactionFactory->create([]);
     }
 
@@ -87,13 +117,15 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
      */
     public function testRefundThrowsExceptionWithoutPayment()
     {
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction());
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction(),
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $transactionFactory->refund([]);
     }
 
     public function testRefundOperationSetter()
     {
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction());
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, new PayPalTransaction(),
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $expected = Operation::CREDIT;
         $this->assertEquals($expected, $transactionFactory->getRefundOperation());
     }
@@ -103,7 +135,8 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
         $transactionMock = $this->getMock(Transaction::class);
         $transactionMock->expects($this->Once())->method('setAmount')->with($this->equalTo(new Amount('1.0', 'EUR')));
 
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock);
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock,
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $transactionFactory->create($this->commandSubject);
     }
 
@@ -113,7 +146,8 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
         $redirect = new Redirect('http://magen.to/frontend/redirect', 'http://magen.to/frontend/cancel', 'http://magen.to/frontend/redirect');
         $transactionMock->expects($this->Once())->method('setRedirect')->with($this->equalTo($redirect));
 
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock);
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock,
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $transactionFactory->create($this->commandSubject);
     }
 
@@ -122,7 +156,8 @@ class TransactionFactoryUTest extends \PHPUnit_Framework_TestCase
         $transactionMock = $this->getMock(Transaction::class);
         $transactionMock->expects($this->Once())->method('setNotificationUrl')->with($this->equalTo('http://magen.to/frontend/notify'));
 
-        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock);
+        $transactionFactory = new TransactionFactory($this->urlBuilder, $this->resolver, $transactionMock,
+            $this->config, $this->storeManager, $this->accountHolderFactory, $this->basketFactory);
         $transactionFactory->create($this->commandSubject);
     }
 }
