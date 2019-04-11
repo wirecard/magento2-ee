39c9c287558244834aff5592568284c355d424cf
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 13:46:13 +0200

    Run UI tests on api-test 2

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -17,4 +17,5 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+echo "Make it executable."
 docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html"
