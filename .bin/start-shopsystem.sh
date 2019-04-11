ae7b1e3c372870ba59771680ed6bb415a160e315
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 12:41:27 +0200

    Take root files out

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -16,5 +16,6 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+echo "START LAST STEP"
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
 docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && ls"
