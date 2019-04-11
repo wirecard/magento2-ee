e6831a33516b29640821e6a2d65810a83f679a3a
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 09:00:04 +0200

    Install shop and install plugin

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -13,3 +13,7 @@ done
 
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
