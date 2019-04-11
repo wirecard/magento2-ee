8fee15753f1f0593a67b3f6ceeea85f5f5a38fc4
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 09:49:36 +0200

    Change permissions

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -17,3 +17,5 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+cd '~/'
+chmod -R 777 /var/www/html
