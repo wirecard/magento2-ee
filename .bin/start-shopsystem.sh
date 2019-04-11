1542a2c933196cfa2929b34cfe39cc79dbc0d698
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 16:04:17 +0200

    Find a configure file

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -17,6 +17,6 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && find /var/www/html/ -name configure_payment_method_db.php"
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && find /var/www/html/ -name configure_payment_method_db.php"
 
-# docker exec ${MAGENTO_CONTAINER_NAME} php tests/_data/configure_payment_method_db.php creditcard
+docker exec ${MAGENTO_CONTAINER_NAME} php tests/_data/configure_payment_method_db.php creditcard
