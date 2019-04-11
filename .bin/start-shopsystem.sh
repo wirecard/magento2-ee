acb1655074cb9e3007013651a1c1e09915129b76
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 08:02:45 +0200

    Install magento shop

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-export MAGENTO_CONTAINER_NAME=magento2
+export MAGENTO_CONTAINER_NAME=web
 
-docker-compose build --build-arg GATEWAY=${GATEWAY} magento2
+docker-compose build --build-arg GATEWAY=${GATEWAY} web
 docker-compose up > /dev/null &
 
 while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
