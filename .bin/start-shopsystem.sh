a01ed05ceead5f93cca09912ca190e08e12edc5f
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-11 11:12:49 +0200

    Check the path

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -17,4 +17,4 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html"
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && pwd"
