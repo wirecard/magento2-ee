f8bec363a5f6aa592d1d8c93bff667ec88d77082
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 10:48:11 +0200

    Install magento2 shop correctly

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -14,15 +14,15 @@ done
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy -f
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento deploy:mode:set developer
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento deploy:mode:set developer
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
-docker exec -it ${MAGENTO_CONTAINER_NAME} rm -rf var/cache var/generation var/pagecache var/di
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && ls && find /root -name configure_payment_method_db.php"
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+#docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html/"
 
 #echo "Executing configure file. ->"
-#docker exec ${MAGENTO_CONTAINER_NAME} php tests/_data/configure_payment_method_db.php creditcard
+#docker exec ${MAGENTO_CONTAINER_NAME} php /var/www/html/_data/configure_payment_method_db.php creditcard
