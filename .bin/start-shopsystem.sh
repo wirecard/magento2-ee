819e3459429a0b8dfa29f04e0411d03294858f58
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 10:08:41 +0200

    Run developer mode 4

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -22,7 +22,7 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && find /var/www/html/ -name configure_payment_method_db.php"
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html && find /root -name configure_payment_method_db.php"
 
-echo "Executing configure file. ->"
-docker exec ${MAGENTO_CONTAINER_NAME} php tests/_data/configure_payment_method_db.php creditcard
+#echo "Executing configure file. ->"
+#docker exec ${MAGENTO_CONTAINER_NAME} php tests/_data/configure_payment_method_db.php creditcard
