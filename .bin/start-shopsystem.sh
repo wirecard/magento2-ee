8d2149592199bce9f8196e73e918d45cfd519c14
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 15:03:38 +0200

    Add to install en_US

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -14,7 +14,7 @@ done
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy -f
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy en_US -f
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
 
