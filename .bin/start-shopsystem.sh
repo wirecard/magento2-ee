6782de8191b9bb4be07e5aadea2afc21197f8fcc
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 10:52:39 +0200

    Install magento2 shop correctly

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -24,5 +24,4 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
 #docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
 #docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /var/www/html/"
 
-#echo "Executing configure file. ->"
 #docker exec ${MAGENTO_CONTAINER_NAME} php /var/www/html/_data/configure_payment_method_db.php creditcard
