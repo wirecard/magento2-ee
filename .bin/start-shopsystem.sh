b455fa42a04a2822a660b70b5db464671c270889
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 15:25:38 +0200

    Add to install en_US

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -11,16 +11,20 @@ while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
     sleep 5
 done
 
-docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
-docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy en_US -f
+docker exec -it ${MAGENTO_CONTAINER_NAME} composer require adyen/module-payment
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:enable Adyen_Payment
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
-
+#docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
+#docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy en_US -f
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
+sleep 30s
 docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
 
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./ && find . -name "_data" -print && find . -name "gateway_configs" -print && find . -name "configure_payment_method_db.php" -print"
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
