f86cf261764589e3882edd8bb88ae6666e3f6e3a
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-12 14:32:13 +0200

    Install shop, wirecard plugin and add permissions

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -22,3 +22,5 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 /root"
