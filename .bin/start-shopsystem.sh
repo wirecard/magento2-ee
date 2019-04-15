bb078d8a5d0e6acef7eb17ed8ea116ace65c9f81
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-15 08:44:05 +0200

    Change php version

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -11,7 +11,6 @@ while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
     sleep 5
 done
 
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 echo "Installing done!"
@@ -20,5 +19,5 @@ sleep 120s
 #docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 #docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 #docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-#
-#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
+
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
