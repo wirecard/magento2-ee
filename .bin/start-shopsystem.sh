cad864d882fd2fa6e71c0a164d394b1031ffbbcf
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-15 08:00:10 +0200

    Add de_DE, remove volumes, change currency and timezone

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -3,27 +3,21 @@ set -e
 
 export MAGENTO_CONTAINER_NAME=web
 
-#docker-compose build --build-arg GATEWAY=${GATEWAY} web
-#docker-compose up > /dev/null &
-#
-#while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
-#    echo "Waiting for docker container to initialize"
-#    sleep 5
-#done
+docker-compose build --build-arg GATEWAY=${GATEWAY} web
+docker-compose up > /dev/null &
 
-curl -s https://raw.githubusercontent.com/clean-docker/Magento2/master/init | bash -s ${MAGENTO_CONTAINER_NAME} clone
-cd ${MAGENTO_CONTAINER_NAME}
+while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
+    echo "Waiting for docker container to initialize"
+    sleep 5
+done
+
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
-#docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
-#docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+sleep 120s
+#docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 #docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:static-content:deploy en_US -f
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:flush
-#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento indexer:reindex
-sleep 30s
-docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
+#docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
+#
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "cd ~/ && chmod -R 777 ./"
