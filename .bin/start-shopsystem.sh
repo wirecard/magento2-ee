7115bba98612edebccb9b92d099213bd1250cb5d
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-15 12:23:30 +0200

    Add sleep 120s and set php 7.0

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -17,5 +17,6 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
 
+sleep 120s
 echo "Make folders writeable and find configuration file"
 docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./ && find . -name configure_payment_method_db"
