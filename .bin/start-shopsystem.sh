da3590c0a309fddaac089b2b18492417b998fec2
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-15 12:30:02 +0200

    Comment finding configure file

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -10,13 +10,16 @@ while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}"); do
     sleep 5
 done
 
+# install magento shop
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-magento
 docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
+
+#install wirecard magento2 plugin
 docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento module:status
-
+docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
 sleep 120s
-echo "Make folders writeable and find configuration file"
-docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./ && find . -name configure_payment_method_db"
+#echo "Make folders writeable and find configuration file"
+#docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./ && find . -name configure_payment_method_db"
