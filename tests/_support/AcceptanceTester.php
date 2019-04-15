0f767f8649788078e059fd57fc6fa3013fd308e0
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-15 15:31:15 +0200

    Add hover and change y offset to 900

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -185,7 +185,8 @@ class AcceptanceTester extends \Codeception\Actor
     {
         $this->iAmOnPage('Shop');
         //chose a product and open product page
-        $this->scrollTo(['class' => 'page-wrapper'], 20, 700);
+        $this->scrollTo(['class' => 'page-wrapper'], 20, 900);
+        $this->moveMouseOver(['css' => '//*[@id="maincontent"]/div[3]/div/div[2]/div[3]/div/div/ol/li[4]']);
         $this->click($this->currentPage->getElement('First Product in the Product List'));
     }
 
