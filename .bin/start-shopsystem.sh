c5c07c55f9bb9dbf8592c4f326e07764389c2d06
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-16 08:58:09 +0200

    Run UI tests

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -19,9 +19,7 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
+docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:clean
 
 echo "Give permissions to load css! - it is mandatory!"
 docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
-
-echo "Sleep for 4 minutes!"
-sleep 4m
