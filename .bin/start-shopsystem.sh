e9f983e8a02ac48033f8edb1d7bedaaac2b872ab
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-16 10:56:55 +0200

    Test 2.2.7

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -19,9 +19,10 @@ docker exec -it ${MAGENTO_CONTAINER_NAME} install-sampledata
 docker exec -it ${MAGENTO_CONTAINER_NAME} composer require wirecard/magento2-ee
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:upgrade
 docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento setup:di:compile
-docker exec -it ${MAGENTO_CONTAINER_NAME} php bin/magento cache:clean
 
 echo "Give permissions to load css! - it is mandatory!"
 docker exec -it ${MAGENTO_CONTAINER_NAME} bash -c "chmod -R 777 ./"
 
 #docker exec ${MAGENTO_CONTAINER_NAME} bash -c "cd tests/_data && php configure_payment_method_db.php creditcard"
+echo "Sleep for 4 minutes"
+sleep 5m
