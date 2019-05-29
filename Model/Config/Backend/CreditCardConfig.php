454cfd52f5f0c359a23a4903d3e1b102c8304d0d
serucee <sebastian.russmann@wirecard.com>
2019-05-29 12:14:49 +0200

    #236 Add notifications to configuration

--- Model/Config/Backend/CreditCardConfig.php
+++ Model/Config/Backend/CreditCardConfig.php
@@ -1,9 +1,32 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: sebastian.russmann
- * Date: 5/29/2019
- * Time: 10:03 AM
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License Version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
  */
 
 namespace Wirecard\ElasticEngine\Model\Config\Backend;
@@ -14,10 +37,36 @@ class CreditCardConfig extends Value
 {
     const ERR_MSG_MIXED_CREDENTIALS = "Attention: Please check your credentials within the URL setting fields. You might have configured/combined a productive account with a test account.";
 
+    private $messageManager;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    )
+    {
+        parent::__construct($context,
+            $registry,
+            $config,
+            $cacheTypeList,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+
+        $this->messageManager = $messageManager;
+    }
+
     public function beforeSave()
     {
+        //possible error, warning, notice, success
         if (!$this->isUrlConfigurationValid()) {
-            throw new \Magento\Framework\Exception\ValidatorException(__(self::ERR_MSG_MIXED_CREDENTIALS));
+            $this->messageManager->addWarningMessage(self::ERR_MSG_MIXED_CREDENTIALS);
         }
 
         parent::beforeSave();
