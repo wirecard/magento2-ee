3fa9efa2ce003f27c38851cac2087e960ac88ba5
rinnhofer <jacqueline.rinnhofer@wirecard.com>
2019-07-18 09:28:26 +0200

    Update missing license headers

--- app/etc/vendor_path.php
+++ app/etc/vendor_path.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento2-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento2-ee/blob/master/LICENSE
+ */
+
+/**
  * Path to Composer vendor directory
  */
 return './vendor';
