c1e4412c560fe8d255efba2fde3e02c427febbd3
Travis CI <wirecard@travis-ci.org>
2019-08-07 15:53:54 +0200

    Change path of .env file

--- .bin/run-ui-tests.sh
+++ .bin/run-ui-tests.sh
@@ -11,7 +11,7 @@ export VERSION=`jq .[0].release SHOPVERSIONS`
 
 
 set -a # automatically export all variables from .env file
-source ../.env
+source .env
 set +a
 
 curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
