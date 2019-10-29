693b9dec37ff59a2ab49bc23edc358f72434624a
Adis Brkic <35455551+brkicadis@users.noreply.github.com>
2019-10-29 16:45:46 +0100

    Check compatibility before update shop release versions (#309)
    * Check compatibility before update shop release versions
    
    * Enable running on commit and activate "debugging" in shell scripts
    
    * Change report branch and run ui tests on pull request

--- .bin/get-latest-shop-release-versions.sh
+++ .bin/get-latest-shop-release-versions.sh
@@ -5,16 +5,18 @@
 # - License can be found under:
 # https://github.com/wirecard/magento2-ee/blob/master/LICENSE
 
+set -e
+set -x
+
 curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/magento/magento2/releases | jq -r '.[] | .tag_name' | egrep -v [a-zA-Z] | head -n3 > tmp.txt
 # sort versions in descending order
-sort -nr tmp.txt > ${MAGENTO2_RELEASES_FILE}
+sort -nr tmp.txt > ${MAGENTO2_COMPATIBILITY_FILE}
 
-if [[ $(git diff HEAD ${MAGENTO2_RELEASES_FILE}) != '' ]]; then
+if [[ $(git diff HEAD ${MAGENTO2_COMPATIBILITY_FILE}) != '' ]]; then
     git config --global user.name "Travis CI"
     git config --global user.email "wirecard@travis-ci.org"
 
-    git add  ${MAGENTO2_RELEASES_FILE}
+    git add  ${MAGENTO2_COMPATIBILITY_FILE}
     git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
     git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master
 fi
-
--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -5,6 +5,9 @@
 # - License can be found under:
 # https://github.com/wirecard/magento2-ee/blob/master/LICENSE
 
+set -e
+set -x
+
 PREVIEW_LINK='https://raw.githack.com/wirecard/reports'
 REPORT_FILE='report.html'
 #choose slack channel depending on the gateway
@@ -23,16 +26,30 @@ curl -X POST -H 'Content-type: application/json' \
     Build Number: ${TRAVIS_BUILD_NUMBER}\n
     Branch: ${BRANCH_FOLDER}', 'channel': '${CHANNEL}'}" ${SLACK_ROOMS}
 
-
-# send link to the report into slack chat room
-curl -X POST -H 'Content-type: application/json' --data "{
-    'attachments': [
-        {
-            'fallback': 'Failed test data',
-            'text': 'There are failed tests.
-             Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
-             All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
-            'color': '#764FA5'
-        }
-    ], 'channel': '${CHANNEL}'
-}"  ${SLACK_ROOMS};
+if [[ ${COMPATIBILITY_CHECK}  == "0" ]]; then
+  # send link to the report into slack chat room
+  curl -X POST -H 'Content-type: application/json' --data "{
+      'attachments': [
+          {
+              'fallback': 'Failed test data',
+              'text': 'There are failed tests.
+               Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
+               All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
+              'color': '#764FA5'
+          }
+      ], 'channel': '${CHANNEL}'
+  }"  ${SLACK_ROOMS};
+else
+  # send link to the report into slack chat room if we are not compatible with the latest released version
+  curl -X POST -H 'Content-type: application/json' --data "{
+      'attachments': [
+          {
+              'fallback': 'Failed test data',
+              'text': 'We are not compatible with the latest Magento2 released version: ${MAGENTO2_VERSION}.
+               Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
+               All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
+              'color': '#764FA5'
+          }
+      ], 'channel': '${CHANNEL}'
+  }"  ${SLACK_ROOMS};
+fi
