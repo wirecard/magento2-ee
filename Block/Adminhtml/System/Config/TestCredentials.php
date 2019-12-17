3e537803a7264ad9bc01cb797e8592a6e0eebac6
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2019-12-17 12:54:21 +0100

    Add file headers

--- Block/Adminhtml/System/Config/TestCredentials.php
+++ Block/Adminhtml/System/Config/TestCredentials.php
@@ -1,7 +1,10 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/magento2-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/magento2-ee/blob/master/LICENSE
  */
 
 namespace Wirecard\ElasticEngine\Block\Adminhtml\System\Config;
