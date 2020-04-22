d747ee99b12616ba5c2f1b78d6faf2e01062716a
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2020-04-22 08:52:45 +0200

    Change ngrok port

--- .bin/start-ngrok.sh
+++ .bin/start-ngrok.sh
@@ -32,7 +32,7 @@ echo "SUBDOMAIN=${SUBDOMAIN}"
 
 # Open ngrok tunnel
 "${PWD}"/ngrok authtoken "${NGROK_TOKEN}"
-"${PWD}"/ngrok http 8080 -subdomain="${SUBDOMAIN}" >/dev/null &
+"${PWD}"/ngrok http 80 -subdomain="${SUBDOMAIN}" >/dev/null &
 NGROK_URL_HTTPS=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
 
 # allow ngrok to initialize
