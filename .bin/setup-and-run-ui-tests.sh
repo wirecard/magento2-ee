ce4ea52ad3722e6cbe504a6d2f4e973650d58d73
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2020-04-24 09:51:14 +0200

    Update file headers

--- .bin/setup-and-run-ui-tests.sh
+++ .bin/setup-and-run-ui-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Shop System Plugins:
+# Shop System Extensions:
 # - Terms of Use can be found under:
 # https://github.com/wirecard/magento2-ee/blob/master/_TERMS_OF_USE
 # - License can be found under:
